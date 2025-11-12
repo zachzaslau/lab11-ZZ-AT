14b9aad17797375259dfdceb33d2f087220e103a
Zach Zaslau <zzaslau@ufl.edu>
2025-11-12 17:10:27 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -7,6 +7,14 @@ One function per operation, in order.
 # First example
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b): return math.hypot(a, b)
+
 def add(a, b): return a + b
 def sub(a, b): return a - b
 def mul(a, b): return a * b
