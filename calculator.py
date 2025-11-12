6a0d0dca69d74aa45740fa0a441226dcc8a950a7
Zach Zaslau <zzaslau@ufl.edu>
2025-11-12 17:05:45 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,23 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+def exp(a, b): return a ** b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
+def log(a, b):
+    if b <= 0 or a <= 0:
+        raise ValueError
+    else:
+        return math.log(b, a)
+
+
 
 
