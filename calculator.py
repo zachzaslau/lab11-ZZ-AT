69d490e1dce49b2203bb2972e61dbfcbd0c236af
ToscanoAnthony <toscanoanthony27@gmail.com>
2025-11-12 17:49:23 -0500

    test

--- calculator.py
+++ calculator.py
@@ -20,7 +20,7 @@ def square_root(a):
 def hypotenuse(a, b): return math.hypot(a, b)
 
 def add(a, b): return a + b
-def sub(a, b): return a - b
+def subtract(a, b): return a - b
 def mul(a, b): return a * b
 def exp(a, b): return a ** b
 def div(a, b):
@@ -28,7 +28,7 @@ def div(a, b):
         raise ZeroDivisionError
     else:
         return b / a
-def log(a, b):
+def logarithm(a, b):
     if b <= 0 or a <= 0:
         raise ValueError
     else:
