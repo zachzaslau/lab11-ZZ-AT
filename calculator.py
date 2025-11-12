59aedfd6aeb1eef45ef1434115d85c1ec5c73db7
Zach Zaslau <zzaslau@ufl.edu>
2025-11-12 18:06:04 -0500

    fix

--- calculator.py
+++ calculator.py
@@ -12,41 +12,27 @@ One function per operation, in order.
 import math
 
 def square_root(a):
-    try:
-        if a < 0:
-            raise ValueError
+    if a < 0:
+        raise ValueError
+    else:
         return math.sqrt(a)
-    except ValueError:
-        return False
 
 def hypotenuse(a, b): return math.hypot(a, b)
 
 def add(a, b): return a + b
-def subtract(a, b): return a - b
+def sub(a, b): return a - b
 def mul(a, b): return a * b
 def exp(a, b): return a ** b
 def div(a, b):
-    try:
-        if a == 0:
-            raise ZeroDivisionError
+    if a == 0:
+        raise ZeroDivisionError
+    else:
         return b / a
-<<<<<<< HEAD
-    except ZeroDivisionError:
-        return False
-
 def log(a, b):
-    try:
-        if b <= 0 or a <= 0:
-            raise ValueError
-=======
-def logarithm(a, b):
     if b <= 0 or a <= 0:
         raise ValueError
     else:
->>>>>>> 822868cb8522e16cacb8b62e28199399b85eff49
         return math.log(b, a)
-    except ValueError:
-        return False
 
 
 
